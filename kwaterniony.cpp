172fdf3e031c8a524f48463fd8293fd91cfff9db
nalesniczek <bartosz.bielawiec1@gmail.com>
2025-03-27 13:10:32 +0100

    poprawione kwaterniony

--- kwaterniony.cpp
+++ kwaterniony.cpp
@@ -1,107 +1,63 @@
 #include <iostream>
 #include <ostream>
+#include <complex>
 
-class kwaternion{
+class Kwaternion{
     private:
         float a, b, c, d;
     public:
-        kwaternion();
-        kwaternion(float,float,float,float);
-        float kwa_matrix();
+        Kwaternion(): a(0), b(0), c(0),d(0){};
+        Kwaternion(float a,float b,float c,float d): a(a), b(b), c(c), d(d){};
+        
+        void print_in_form_Matrix_4x4();
+        void print_in_form_Matrix_2x2();
 
-        float get_a();
-        float get_b();
-        float get_c();
-        float get_d();
 
-        void set_a(float);
-        void set_b(float);
-        void set_c(float);
-        void set_d(float);
+        friend Kwaternion operator*(Kwaternion & H1,Kwaternion & H2);
+        friend Kwaternion operator+(Kwaternion & H1,Kwaternion & H2);
 
-        friend kwaternion operator*(kwaternion & H1,kwaternion & H2);
-        friend kwaternion operator+(kwaternion & H1,kwaternion & H2);
-
+        friend std::ostream& operator<<(std::ostream& wyjscie, const Kwaternion& z);
 
 };
 
-kwaternion::kwaternion(float x,float y,float z,float t){
-    a = x;
-    b = y;
-    c = z;
-    d = t;
+void Kwaternion::print_in_form_Matrix_4x4(){
+    std::cout 
+    << a << " " << -b << " " << -c << " " << -d << "\n"
+    << b << " " << a << " " << -d << " " << c << "\n"
+    << c << " " << d << " " << a << " " << -b << "\n"
+    << d << " " << -c << " " << b << " " << a << "\n";
+        
 }
-
-kwaternion::kwaternion(){
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
-}
-
-float kwaternion::get_a(){
-    return a;
-}
-
-float kwaternion::get_b(){
-    return b;
-}
-
-float kwaternion::get_c(){
-    return c;
-}
-
-float kwaternion::get_d(){
-    return d;
-}
-
-void kwaternion::set_a(float x){
-    a = x;
-}
-
-void kwaternion::set_b(float x){
-    b = x;
-}
-
-void kwaternion::set_c(float x){
-    c = x;
-}
-
-void kwaternion::set_d(float x){
-    d = x;
-}
-
 
 //dodawanie kwaterionów
-kwaternion operator+(kwaternion & H1, kwaternion & H2){
-    kwaternion HD(H1.a+H2.a, H1.b+H2.b, H1.c+H2.c, H1.c+H2.c);
+Kwaternion operator+(Kwaternion & H1, Kwaternion & H2){
+    Kwaternion HD(H1.a+H2.a, H1.b+H2.b, H1.c+H2.c, H1.c+H2.c);
     return HD;
 }
 
 //mnożenie kwaterionów
-kwaternion operator*(kwaternion & H1, kwaternion & H2){
-    kwaternion HD;
-    HD.set_a(H1.a * H2.a - H1.b * H2.b - H1.c * H2.c - H1.d * H2.d);
-    HD.set_b(H1.a * H2.b + H1.b * H2.a + H1.c * H2.d - H1.d * H2.c);
-    HD.set_c(H1.a * H2.c + H1.c * H2.a + H1.d * H2.b - H1.b * H2.d);
-    HD.set_d(H1.a * H2.d + H1.d * H2.a + H1.b * H2.c - H1.c * H2.b);
-    return HD;
+Kwaternion operator*(Kwaternion & H1, Kwaternion & H2){
+    return Kwaternion(
+    H1.a * H2.a - H1.b * H2.b - H1.c * H2.c - H1.d * H2.d, //wspol a
+    H1.a * H2.b + H1.b * H2.a + H1.c * H2.d - H1.d * H2.c, //wspol b
+    H1.a * H2.c + H1.c * H2.a + H1.d * H2.b - H1.b * H2.d, //wspol c
+    H1.a * H2.d + H1.d * H2.a + H1.b * H2.c - H1.c * H2.b);//wspol d
+
 }
 
-std::ostream & operator<< (std::ostream &wyjscie, kwaternion z){
-    return wyjscie <<  z.get_a() << " + " <<  z.get_b() << "i + " <<  z.get_c() << "j + " <<  z.get_d() << "k"<<"\n";
+std::ostream & operator<< (std::ostream &wyjscie, const Kwaternion & z){
+    return wyjscie <<  z.a << " + " <<  z.b << "i + " <<  z.c << "j + " <<  z.d << "k"<<"\n";
 }
 
 int main() {
-    kwaternion h1(2.0, 4.0, 6.0, 7.0);
-    kwaternion h2(2.0, 7.0, 8.0, 9.0);
+    Kwaternion h1(2.0, 4.0, 6.0, 7.0);
+    Kwaternion h2(2.0, 7.0, 8.0, 9.0);
     std::cout << h1 << "\n";
     std::cout << h2 << "\n";
-    kwaternion h3 = h1*h2;
+    Kwaternion h3 = h1*h2;
     std::cout << h3 << "\n";
+    h3.print_in_form_Matrix_4x4();
 
-
-    std::cout << "Mam dosc windowsa";
     return 0;
 
 }
